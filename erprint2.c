790cfa713fa802ee2f75c8f592d9e94d8f6e7d4e
Mahderp1 <122852903+Mahderp@users.noreply.github.com>
2023-04-26 00:18:55 +0300

    Update erprint2.c

--- erprint2.c
+++ erprint2.c
@@ -1,11 +1,8 @@
-/*
- * File: erprint2
- * Auth: Bereket Zeselassie
- *       Mahder Gebremikael
- */
-
 #include "shell.h"
-
+/**
+ * _eputs - void func
+ * @s: char var
+ */
 void _eputs(char *s)
 {
 	int i = 0;
@@ -15,7 +12,11 @@ void _eputs(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 		_eputchar(s[i]);
 }
-
+/**
+ * _eputchar - func
+ * @c: char var
+ * Return: 1
+ */
 int _eputchar(char c)
 {
 	static int i;
@@ -30,7 +31,12 @@ int _eputchar(char c)
 		b[i++] = c;
 	return (1);
 }
-
+/**
+ * _putfd - func
+ * @c: char var
+ * @f: int var
+ * Return: 1
+ */
 int _putfd(char c, int f)
 {
 	static int i;
@@ -45,7 +51,12 @@ int _putfd(char c, int f)
 		b[i++] = c;
 	return (1);
 }
-
+/**
+ * _putsfd - func
+ * @s: char var
+ * @f: int var
+ * Return: i
+ */
 int _putsfd(char *s, int f)
 {
 	int i = 0;
