494d9e076e0121eb55b49508b6876ead86dbe86e
Mahderp1 <122852903+Mahderp@users.noreply.github.com>
2023-04-25 14:13:16 +0300

    Simple Shell

---
+++ utils.c
@@ -0,0 +1,56 @@
+/*
+ * File: utils
+ * Auth: Bereket Zeselassie
+ *       Mahder Gebremikael
+ */
+
+#include "shell.h"
+
+int interactive(inf_o *inf)
+{
+	return (isatty(STDIN_FILENO) && inf->readfd <= 2);
+}
+
+int is_delim(char c, char *l)
+{
+	while (*l)
+		if (*l++ == c)
+			return (1);
+	return (0);
+}
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+int _atoi(char *s)
+{
+	int i, n = 1, f = 0, t;
+	unsigned int r = 0;
+
+	for (i = 0;  s[i] != '\0' && f != 2; i++)
+	{
+		if (s[i] == '-')
+			n *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			f = 1;
+			r *= 10;
+			r += (s[i] - '0');
+		}
+		else if (f == 1)
+			f = 2;
+	}
+
+	if (n == -1)
+		t = -r;
+	else
+		t = r;
+
+	return (t);
+}
